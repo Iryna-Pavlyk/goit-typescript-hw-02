28b49e8d1eb4bc19999d9e4364469ac4023cf95b
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2024-05-26 16:50:37 +0300

    up

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ ReactModal.setAppElement("#root");
 import "modern-normalize";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
